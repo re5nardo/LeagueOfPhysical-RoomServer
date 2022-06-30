305e155fcf4109141a5753133ff9878cbb67a407
re5nardo <re5nardo@naver.com>
2022-06-30 21:55:11 +0900

    BIN_PATH 추가

--- RoomServer/src/config/index.ts
+++ RoomServer/src/config/index.ts
@@ -5,3 +5,4 @@ export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR } = process.env;
 export const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 export const { CACHE_HOST, CACHE_PORT } = process.env;
+export const { BIN_PATH } = process.env;
