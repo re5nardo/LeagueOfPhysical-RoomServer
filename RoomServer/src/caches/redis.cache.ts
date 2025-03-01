dbf80cb7c9c3a04025c223e9e666aff78543a929
re5nardo <re5nardo@naver.com>
2025-03-01 16:57:01 +0900

    RedisCache 추가

---
+++ RoomServer/src/caches/redis.cache.ts
@@ -0,0 +1,41 @@
+import { redisClient } from '@loaders/redis.loader';
+
+export class RedisCache {
+    public static async set<T>(key: string, value: T, ttl?: number): Promise<void> {
+        try {
+            const data = JSON.stringify(value);
+            const options = ttl ? { EX: ttl } : undefined;
+            await redisClient.set(key, data, options);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    public static async get<T>(key: string): Promise<T | null> {
+        try {
+            const data = await redisClient.get(key);
+            if (!data) {
+                return null;
+            }
+            return JSON.parse(data) as T;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    public static async delete(key: string): Promise<void> {
+        try {
+            await redisClient.del(key);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    public static async exists(key: string): Promise<boolean> {
+        try {
+            return (await redisClient.exists(key)) === 1;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+}
